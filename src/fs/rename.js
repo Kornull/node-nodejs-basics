22f813f05f58d96290a3568ff65a054f1bef6064
Kornull <wolod1987kor@gmail.com>
2023-06-13 15:59:52 +0300

    feat: add fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import fs, { constants } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FOLDER = 'files';
+const FILE = 'wrongFilename.txt';
+const RENAME_FILE = 'wrongFilename.md';
+const PATH_TO_FILE = join(__dirname, FOLDER, FILE);
+const PATH_TO_RENAME_FILE = join(__dirname, FOLDER, RENAME_FILE);
+
 const rename = async () => {
-    // Write your code here 
+  fs.access(PATH_TO_FILE, constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.access(PATH_TO_RENAME_FILE, constants.F_OK, (err) => {
+        if (err) {
+          fs.rename(PATH_TO_FILE, PATH_TO_RENAME_FILE, (err) => {
+            if (err) throw new Error('FS operation failed');
+          });
+        } else {
+          throw new Error('FS operation failed');
+        }
+      });
+    }
+  });
 };
 
 await rename();
