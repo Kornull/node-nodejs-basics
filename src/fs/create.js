c247f7d7f343af76d60450fb928edc071f1c1655
Kornull <wolod1987kor@gmail.com>
2023-06-13 15:34:04 +0300

    feat: add fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FOLDER = 'files';
+const FILE = 'fresh.txt';
+const PATH_TO_FILE = join(__dirname, FOLDER, FILE);
+const MESSAGE = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.writeFile(PATH_TO_FILE, MESSAGE, { flag: 'wx' });
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
