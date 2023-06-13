67b3e7c8c06d2677b03bcd2f810e548bb179ff88
Kornull <wolod1987kor@gmail.com>
2023-06-13 15:41:51 +0300

    feat: add fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import fs from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FOLDER = 'files';
+const COPY_FOLDER = 'files_copy';
+const PATH_TO_FOLDER = join(__dirname, FOLDER);
+const PATH_TO_NEW_FOLDER = join(__dirname, COPY_FOLDER);
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.access(PATH_TO_FOLDER);
+    try {
+      await fs.access(PATH_TO_NEW_FOLDER);
+      throw new Error();
+    } catch {
+      await fs.mkdir(PATH_TO_NEW_FOLDER);
+      const files = await fs.readdir(PATH_TO_FOLDER);
+      files.forEach(async (file) => {
+        await fs.copyFile(
+          join(PATH_TO_FOLDER, file),
+          join(PATH_TO_NEW_FOLDER, file)
+        );
+      });
+    }
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
