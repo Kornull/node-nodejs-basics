9e3e75bfbe10d0d0847823388bf51856fd39acc0
Kornull <wolod1987kor@gmail.com>
2023-06-13 16:05:02 +0300

    feat: add fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FOLDER = 'files';
+const FILE = 'fileToRemove.txt';
+
+const PATH_TO_FOLDER = join(__dirname, FOLDER, FILE);
+
 const remove = async () => {
-    // Write your code here 
+  try {
+   await fs.unlink(PATH_TO_FOLDER);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
