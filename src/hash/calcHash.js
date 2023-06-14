7db995dcd7912ec0cba1c621c6c7f282255cce29
Kornull <wolod1987kor@gmail.com>
2023-06-14 11:16:41 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import crypto from 'node:crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FOLDER = 'files';
+const FILE = 'fileToCalculateHashFor.txt';
+const PATH_TO_FILE = join(__dirname, FOLDER, FILE);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = await fs.readFile(PATH_TO_FILE, 'utf-8');
+  const hashResult = crypto.createHash('sha256').update(file).digest('hex');
+
+  console.log(hashResult);
 };
 
 await calculateHash();
