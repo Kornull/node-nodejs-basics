36bf07ddc7399fa2e91f75f3ea2712bf2b0f4b86
Kornull <wolod1987kor@gmail.com>
2023-06-15 18:04:32 +0300

    feat: add zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createGunzip } from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FOLDER = 'files';
+const FILE = 'fileToCompress.txt';
+const GZ = 'archive.gz';
+const PATH_TO_FOLDER = join(__dirname, FOLDER);
+
 const decompress = async () => {
-    // Write your code here 
+  const rs = fs.createReadStream(join(PATH_TO_FOLDER, GZ))
+  const ws = fs.createWriteStream(join(PATH_TO_FOLDER, FILE))
+  await pipeline(rs, createGunzip(), ws)
 };
 
 await decompress();
