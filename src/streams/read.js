53671690b3723fcc28827266b0112c9c5f855200
Kornull <wolod1987kor@gmail.com>
2023-06-15 16:58:43 +0300

    feat: read/write stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import process from 'process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FOLDER = 'files';
+const FILE = 'fileToRead.txt';
+const PATH_TO_FILE = join(__dirname, FOLDER, FILE);
+
 const read = async () => {
-    // Write your code here 
+  const rs = fs.createReadStream(PATH_TO_FILE, 'utf-8');
+  rs.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import process from 'process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FOLDER = 'files';
+const FILE = 'fileToWrite.txt';
+const PATH_TO_FILE = join(__dirname, FOLDER, FILE);
+
+
 const write = async () => {
-    // Write your code here 
+    const ws = fs.createWriteStream(PATH_TO_FILE, 'utf-8')
+    process.stdin.pipe(ws)
 };
 
 await write();
