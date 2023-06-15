296d8be643c78dfc0171b7724f23b7c2dbcacf53
Kornull <wolod1987kor@gmail.com>
2023-06-15 17:25:06 +0300

    feat: add transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform, pipeline } from 'stream';
+import process from 'process';
+
 const transform = async () => {
-    // Write your code here 
+  const streamTransform = new Transform({
+    transform(chunk, enc, callback) {
+      const res = chunk.toString().trim().split('').reverse().join('');
+
+      callback(null, `${res}\n`);
+    },
+  });
+
+  pipeline(process.stdin, streamTransform, process.stdout, (err) =>
+    console.log(err)
+  );
 };
 
 await transform();
